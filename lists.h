7b36fc2f3583be484a57ce317c39deb9e2a6a0e8
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-15 17:43:17 +0100

    Update lists.h

--- lists.h
+++ lists.h
@@ -1,11 +1,18 @@
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
 
-/**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include "monty.h"
+
+/* list_funcs1.c */
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+size_t print_dlistint(const dlistint_t *h);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+/* list_funcs2.c */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+
+#endif

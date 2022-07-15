c29bc2f6b794241de319e072833eb855b36fa24e
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-07-15 19:08:52 +0100

    Update free.c

--- free.c
+++ free.c
@@ -1,74 +1,31 @@
 #include "monty.h"
 #include "lists.h"
 
-data_t data = DATA_INIT;
-
 /**
- * monty - helper function for main function
- * @args: pointer to struct of arguments from main
- *
- * Description: opens and reads from the file
- * containing the opcodes, and calls the function
- * that will find the corresponding executing function
+ * free_all - custom memory handling function
+ * @all: flag to indicate what to free
  */
-void monty(args_t *args)
+void free_all(int all)
 {
-	size_t len = 0;
-	int get = 0;
-	void (*code_func)(stack_t **, unsigned int);
+	if (data.line)
+	{
+		free(data.line);
+		data.line = NULL;
+		free_everything(data.words);
+		data.words = NULL;
+	}
 
-	if (args->ac != 2)
+	if (all)
 	{
-		dprintf(STDERR_FILENO, USAGE);
-		exit(EXIT_FAILURE);
-	}
-	data.fptr = fopen(args->av, "r");
-	if (!data.fptr)
-	{
-		dprintf(STDERR_FILENO, FILE_ERROR, args->av);
-		exit(EXIT_FAILURE);
-	}
-	while (1)
-	{
-		args->line_number++;
-		get = getline(&(data.line), &len, data.fptr);
-		if (get < 0)
-			break;
-		data.words = strtow(data.line);
-		if (data.words[0] == NULL || data.words[0][0] == '#')
+		if (data.stack)
 		{
-			free_all(0);
-			continue;
+			free_dlistint(data.stack);
+			data.stack = NULL;
 		}
-		code_func = get_func(data.words);
-		if (!code_func)
+		if (data.fptr)
 		{
-			dprintf(STDERR_FILENO, UNKNOWN, args->line_number, data.words[0]);
-			free_all(1);
-			exit(EXIT_FAILURE);
+			fclose(data.fptr);
+			data.fptr = NULL;
 		}
-		code_func(&(data.stack), args->line_number);
-		free_all(0);
 	}
-	free_all(1);
-}
-
-/**
- * main - entry point for monty bytecode interpreter
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE
- */
-int main(int argc, char *argv[])
-{
-	args_t args;
-
-	args.av = argv[1];
-	args.ac = argc;
-	args.line_number = 0;
-
-	monty(&args);
-
-	return (EXIT_SUCCESS);
 }
